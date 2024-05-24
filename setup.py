fc7c00a1984560aac8c439068fcc4528966afd2d
jakubguzek <jakub.j.guzek@gmail.com>
2024-05-24 11:20:19 +0200

    fix: executing of entry point

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "euglenins = euglenida:euglenins.py",
+            "euglenins = euglenida:euglenins",
         ]
     },
 )
